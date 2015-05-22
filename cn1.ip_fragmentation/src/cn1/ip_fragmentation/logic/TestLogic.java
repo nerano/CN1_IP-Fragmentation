e1f60382277b2f2f7c47a6d061dae3251b95af37
nerano <sebastian.kropp@web.de>
2015-05-22 23:55:33 +0200

    Input header length + upgrade frame length (check "update" in fragmentation description for more details)

--- cn1.ip_fragmentation/src/cn1/ip_fragmentation/logic/TestLogic.java
+++ cn1.ip_fragmentation/src/cn1/ip_fragmentation/logic/TestLogic.java
@@ -18,7 +18,7 @@ public class TestLogic {
 	
 	private int packageLength;
 	private Integer frameLength;
-	private int headerLength = 20;
+	private Integer headerLength;
 	private String identifier = "0000";
 	
 	private ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
@@ -29,23 +29,50 @@ public class TestLogic {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String length = reader.readLine();
 		this.packageLength = Integer.parseInt(length);
+		
 		System.out.println("Please put in the length for each Frame (also in byte): ");
 		length = reader.readLine();
 		this.frameLength = Integer.parseInt(length);
+		/**
+		 * Einlesen der Header Länge. Ich lasse den Nutzer hier nur words eingeben, da die Header Länge durch 8
+		 * teilbar bleiben muss. Wenn wir hier ebenfalls bytes eingeben lassen, kann etwas komisches wie 25 oder
+		 * so ohne Probleme eingelesen werden. Durch das einlesen und umrechnen der Wörter kann dies nicht passieren.
+		 */
+		System.out.println("Please put in the header length (length between 5 words = 20 bytes"
+				+ " and 15 words = 60 bytes. The input should be in words!): ");
+		length = reader.readLine();
+		if(Integer.parseInt(length) < 5 || Integer.parseInt(length) > 15) {
+			throw new IllegalArgumentException("Wrong input! The input value has to be between 5 and 20!");
+		} else {
+			this.headerLength = Integer.parseInt(length)*32/8;
+		}
 	}
 	
 	/**
 	 * Diese Methode berechnet den "Offset" und teilt das package in frames auf.
 	 * Zur Verdeutlichung habe ich noch ein Bild ins repository gestellt, das zeigt was genau hier passiert.
+	 * Update: Ich habe nun auch die Header Länge eingebaut und einen Fehler in der Berechnung der frame length
+	 * 		   für das letzte Frame behoben. Der Programmierstiel ist mittlerweile natürlich ziemlich hässlich,
+	 * 		   aber die Logik passt.
 	 */
 	private void fragmentation() {
 		int packNum = (this.packageLength/this.frameLength) + 1;
+		int totalProcessed = 0;
+		Integer frameLength = 0;
 		Integer offset = 0;
 				
 		for (int i = 0; i < packNum; i++) {
 			map = new HashMap<String, String>();
+			
 			map.put("ID", this.identifier);
-			map.put("length", this.frameLength.toString());
+			
+			if (totalProcessed > (this.packageLength-this.frameLength-this.headerLength)) {
+				frameLength = this.packageLength-totalProcessed;
+				map.put("length", frameLength.toString());
+			} else {
+				map.put("length", this.frameLength.toString());
+			}
+			
 			if (i < packNum-1) {
 				map.put("MF", "1");
 			} else {
@@ -59,6 +86,7 @@ public class TestLogic {
 				map.put("offset", "0");
 			}
 			this.list.add(map);
+			totalProcessed += this.frameLength-this.headerLength;
 		}
 		
 		// Ich war mir nicht sicher, ob wir die ID immer brauchen, diese ist nämlich nur sinnvoll, wenn mehr als
@@ -68,8 +96,10 @@ public class TestLogic {
 	}
 	
 	private void output() {
+		System.out.println("Header length: " + this.headerLength + " bytes.");
 		System.out.println(list.size()+" frames were created.\n");
 		System.out.println("ID\tlength\tMF\toffset");
+		
 		for(int i = 0; i < this.list.size(); i++) {
 			System.out.print(list.get(i).get("ID")+"\t");
 			System.out.print(list.get(i).get("length")+"\t");
